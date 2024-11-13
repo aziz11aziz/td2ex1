9451e0d1cb663ae3918b9c4bac63038e080a23cb
Med Aziz Baccouri <153022894+aziz11aziz@users.noreply.github.com>
2024-11-13 14:02:53 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express(); 
+// Middleware de journalisation 
+const loggerMiddleware = (req, res,next)=>{
+    const now = new Date();
+    const date = now. toLocaleDateString();
+    const time = now. toLocaleTimeString();
+    console.log(`[${date} ${time}] ${req.method}${req.path}`); 
+    next();
+}
+
+// Utiliser le middleware dans l'application 
+app. use(loggerMiddleware);
+// Exemple de route pour tester le middleware 
+app.get('/', (req, res) => 
+      {res.send('Exercice 1!');});
+
+app.listen (3000, ()=> {
+      console.log('Serveur démarré sur le port 3000');
+}); 
